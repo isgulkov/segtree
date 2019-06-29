022594f1f69b0dfa1db9fac833b66a218a80c5d9
Ilya Gulkov <x@isgulkov.ru>
2019-06-30 01:07:47 +0300

    Run benchmarks on random data, add ones for the creation

--- benchmark/rmq.cpp
+++ benchmark/rmq.cpp
@@ -1,3 +1,5 @@
+#include <random>
+
 #include <benchmark/benchmark.h>
 
 #include "baseline/rmq_slow.hpp"
@@ -5,29 +7,54 @@
 #include "rmq_compact.hpp"
 #include "rmq_cartesian.hpp"
 
-template<typename RMQ>
-void BM_RMQ_Bad(benchmark::State& state) {
-    std::vector<int> xs;
-    xs.reserve(state.range(0));
+std::vector<int> random_ints(size_t n)
+{
+    std::mt19937 rng(std::random_device{}());
+    std::binomial_distribution<int> dist(250, 0.25);
 
-    for(int i = 0; i < state.range(0); i++) {
-        xs.push_back(i);
+    std::vector<int> xs(n);
+
+    for(int& x : xs) {
+        x = dist(rng) - 150;
     }
+
+    return xs;
+}
+
+template<typename RMQ>
+void BM_RMQ_RandomCreate(benchmark::State& state) {
+    std::vector<int> xs = random_ints(state.range(0));
+
+    for(auto _ : state) {
+        RMQ rmq(xs);
+
+        benchmark::DoNotOptimize(rmq.get(0, 1));
+    }
+}
+
+template<typename RMQ>
+void BM_RMQ_RandomAll(benchmark::State& state) {
+    std::vector<int> xs = random_ints(state.range(0));
 
     RMQ rmq(xs);
 
     for(auto _ : state) {
-        for(int i = 0; i < state.range(0); i++) {
-            for(int j = i + 1; j < state.range(0); j++) {
+        for(int i = 0; i < xs.size(); i++) {
+            for(int j = i + 1; j < xs.size(); j++) {
                 benchmark::DoNotOptimize(rmq.get(i, j));
             }
         }
     }
 }
 
-BENCHMARK_TEMPLATE(BM_RMQ_Bad, seg::baseline::rmq_slow<int>)->Range(64, 4 * 1024)->Unit(benchmark::kMillisecond);
-BENCHMARK_TEMPLATE(BM_RMQ_Bad, seg::rmq_compact<int>)->Range(64, 4 * 1024)->Unit(benchmark::kMillisecond);
-BENCHMARK_TEMPLATE(BM_RMQ_Bad, seg::rmq_fast<int>)->Range(64, 4 * 1024)->Unit(benchmark::kMillisecond);
-BENCHMARK_TEMPLATE(BM_RMQ_Bad, seg::rmq_cartesian<int>)->Range(64, 4 * 1024)->Unit(benchmark::kMillisecond);
+BENCHMARK_TEMPLATE(BM_RMQ_RandomCreate, seg::baseline::rmq_slow<int>)->Range(1024, 256 * 1024)->Unit(benchmark::kMillisecond);
+BENCHMARK_TEMPLATE(BM_RMQ_RandomCreate, seg::rmq_compact<int>)->Range(1024, 256 * 1024)->Unit(benchmark::kMillisecond);
+BENCHMARK_TEMPLATE(BM_RMQ_RandomCreate, seg::rmq_fast<int>)->Range(1024, 256 * 1024)->Unit(benchmark::kMillisecond);
+BENCHMARK_TEMPLATE(BM_RMQ_RandomCreate, seg::rmq_cartesian<int>)->Range(1024, 256 * 1024)->Unit(benchmark::kMillisecond);
+
+BENCHMARK_TEMPLATE(BM_RMQ_RandomAll, seg::baseline::rmq_slow<int>)->Range(64, 4 * 1024)->Unit(benchmark::kMillisecond);
+BENCHMARK_TEMPLATE(BM_RMQ_RandomAll, seg::rmq_compact<int>)->Range(64, 4 * 1024)->Unit(benchmark::kMillisecond);
+BENCHMARK_TEMPLATE(BM_RMQ_RandomAll, seg::rmq_fast<int>)->Range(64, 4 * 1024)->Unit(benchmark::kMillisecond);
+BENCHMARK_TEMPLATE(BM_RMQ_RandomAll, seg::rmq_cartesian<int>)->Range(64, 4 * 1024)->Unit(benchmark::kMillisecond);
 
 BENCHMARK_MAIN();
